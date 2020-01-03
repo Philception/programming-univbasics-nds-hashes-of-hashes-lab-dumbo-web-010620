ab44f0380442daacaa5ca92d383f5cf6d26bfe72
Phillip Henry III <philtrader71@gmail.com>
2020-01-03 18:23:23 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -4,27 +4,28 @@
 #
 # The tests will guide your construction.
 #
-# PORTION_1 = {
-#   label: "Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class"
-#     }
+# Kingdom =
+#   :sub_category=> {
+#     :label=> "Phylum",
+#     :sub_category=> {
+#     :label=> "Class"
 #   }
 # }
-# 
+#
+# naming_system = PORTION_1
+# naming_system
+#
 # PORTION_2 = {
 #   label: "Order"
 # }
-# 
+#
 # PORTION_3 = {
 #   label: "Family",
 #   sub_category: {
 #     label: "Genus",
 #   }
 # }
-# 
+#
 # PORTION_4 = {
 #   label: "Species",
 #   sub_category: nil
@@ -43,3 +44,4 @@ def naming_system
   # HoH all the way to Species!
 
 end
+#
